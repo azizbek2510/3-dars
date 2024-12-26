8b1d121f1b498dbe87675f70066d8e462a6fa242
azizbek2510 <azizbekaliyev1011@gmail.com>
2024-12-26 17:04:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+let num1 = Math.floor(Math.random() * 100) + 1; 
+let num2 = Math.floor(Math.random() * 100) + 1; 
+let product = num1 * num2;
+let dividedResult = product / 2;
+alert(`Числа: ${num1} и ${num2}`);
+alert(`Их произведение: ${product}`);
+alert(`Результат деления на 2: ${dividedResult}`);
+
+
